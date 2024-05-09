e65ad6d84caad9f669786ffee1d39b91228b3e5e
Ziroll <jsparlingzi3757@gmail.com>
2024-05-09 18:43:22 -0500

    added assets folder html script.js and style.css

---
+++ assets/script.js
@@ -0,0 +1,8 @@
+const apiKey = "ecef5ad8043c8002cf9ddea36591b218";
+const weatherNow = document.getElementById("weatherNow");
+const dayContainer = document.getElementById("dayContainer");
+const submit = document.getElementById("submit");
+
+
+
+const apiURL = `api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid=${ecef5ad8043c8002cf9ddea36591b218}`;
