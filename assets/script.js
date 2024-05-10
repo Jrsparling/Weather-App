d699bdad144c9c9bc7bb901d6cbe37a1f3a2810b
Ziroll <jsparlingzi3757@gmail.com>
2024-05-09 20:00:13 -0500

    Updated the README and script.js

--- assets/script.js
+++ assets/script.js
@@ -4,5 +4,5 @@ const dayContainer = document.getElementById("dayContainer");
 const submit = document.getElementById("submit");
 
 
-
-const apiURL = `api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid=${ecef5ad8043c8002cf9ddea36591b218}`;
+const apiURL = `api.openweathermap.org/data/2.5/forecast?q={cityName}&appid={ecef5ad8043c8002cf9ddea36591b218}`;
+// const apiURL = `api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid=${ecef5ad8043c8002cf9ddea36591b218}`;
